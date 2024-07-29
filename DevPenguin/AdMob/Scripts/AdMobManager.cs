05edeb411ce0bf33ae334fbe19f0b578ab614ad4
Eduardo Seitz <eduardoseitz@outlook.com.br>
2024-07-29 00:59:17 -0300

    Update AdMobManager.cs

--- DevPenguin/AdMob/Scripts/AdMobManager.cs
+++ DevPenguin/AdMob/Scripts/AdMobManager.cs
@@ -1,5 +1,6 @@
-using GoogleMobileAds.Api;
+using System;
 using UnityEngine;
+using GoogleMobileAds.Api;
 
 namespace DevPenguin.Ads
 {
@@ -7,157 +8,205 @@ namespace DevPenguin.Ads
     /* Test ad units: https://developers.google.com/admob/android/test-ads */
     public class AdMobManager : MonoBehaviour
     {
+        
         #region Declarations
-        public static AdMobManager instance;
 
+        private const string TAG = "AdMobManager";
+        public static AdMobManager Instance;
+
+        [Header("Debug Settings")]
+        [SerializeField] private bool areLogsEnabled;
         /* Test app id ca-app-pub-3940256099942544~3347511713 */
-        /* Test interstitial ca-app-pub-3940256099942544/1033173712 */
+        
+        [Header("Admob Settings")]
+        [SerializeField] private bool shouldShowAds = true;
+        /* Test Interstitial ca-app-pub-3940256099942544/1033173712 */
         [SerializeField] private string interstitialUnitId = "ca-app-pub-3940256099942544/1033173712";
         /* Test rewarded ca-app-pub-3940256099942544/5224354917 */
         [SerializeField] private string rewardedUnitId = "ca-app-pub-3940256099942544/5224354917";
-        [SerializeField] private bool shouldRewardOnAdClosed = false;
-
-        private InterstitialAd interstitialAd;
-        private RewardedAd rewardedAd;
+        [SerializeField] private bool shouldRewardOnAdClosedBeforeFinish;
+        [Range(1, 600)]
+        [SerializeField] private float showRewardAfterSeconds = 300;
+        
+        // Other private properties.
+        private InterstitialAd _interstitialAd;
+        private RewardedAd _rewardedAd;
+        private float _rewardStartTime;
+        
         #endregion
 
-        #region Getters
+        #region Getters and setters
+
+        public bool ShouldShowAds => shouldShowAds;
         public bool IsReady { get; private set; }
         public bool IsInterstitialAdReady { get; private set; }
         public bool IsRewardedAdReady { get; private set; }
         public bool IsShowingRewardedAd { get; private set; }
-
         public bool AreAdsRemoved { get; private set; }
+        // TODO:
+        public bool IsTimeToShowAds => Time.time >= _rewardStartTime + showRewardAfterSeconds;
+        
         #endregion
 
         #region Events
+        
         public delegate void ReceiveReward();
         public event ReceiveReward OnReceiveReward;
+        
         #endregion
 
         #region MonoBehaviour Methods
+        
         private void Awake()
         {
-            // Singleton setup
-            if (instance)
+            ResetRewardStartTime();
+            
+            // Singleton pattern.
+            if (Instance)
                 Destroy(gameObject);
             else
-                instance = this;
+                Instance = this;
 
-            // Make this scene persistent
-            DontDestroyOnLoad(this);
+            // Make this scene persistent.
+            DontDestroyOnLoad(gameObject);
         }
 
-        // Start is called before the first frame update
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
         private void Start()
         {
-            // Initialize google's admob connection
+            // Initialize google's admob connection.
             MobileAds.Initialize(initStatus =>
             {
-                Debug.Log("Admob initialized sucessfully.");
-
                 IsReady = true;
-
+                
+                // Request ads.
                 if (interstitialUnitId != string.Empty)
-                    RequestInterstititalAd();
-
+                    RequestInterstitialAd();
                 if (rewardedUnitId != string.Empty)
                     RequestRewardedAd();
+                    
+                // Debug.
+                if (areLogsEnabled)
+                    Debug.Log($"{TAG}: Admob initialized successfully.");
             });
 
+            // Test rewarded ad.
+            //ShowRewardedAd();
+            
+            // Test Interstitial ad.
+            //ShowInterstitialAd();
+            
             // TODO: Request and show banner ad
         }
+        
         #endregion
 
         #region Helper Methods
+        
         public void RemoveAds()
         {
             AreAdsRemoved = true;
             IsReady = false;
         }
+        
+        public void ResetRewardStartTime()
+        {
+            // TODO:
+            _rewardStartTime = Time.time;
+        }
 
         #region Interstitial Ad Methods
-        // A insterstitial ad ussually takes 50s to show
-        public void RequestInterstititalAd()
+        
+        /// <summary>
+        /// An interstitial ad usually takes 50 seconds to load.
+        /// </summary>
+        public void RequestInterstitialAd()
         {
-            // Uncoment to debug
-            //Debug.Log("Requesting interstitial ad.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Requesting Interstitial ad.");
 
-            // Initialize an interstitial add
-            interstitialAd = new InterstitialAd(interstitialUnitId);
+            // Initialize an Interstitial add.
+            _interstitialAd = new InterstitialAd(interstitialUnitId);
 
             // Create an empty ad request
             AdRequest adRequest = new AdRequest.Builder().Build();
 
             // Called when an ad request has successfully loaded.
-            interstitialAd.OnAdLoaded += InterstitialAd_OnAdLoaded;
+            _interstitialAd.OnAdLoaded += InterstitialAd_OnAdLoaded;
 
-            // Load the interstitial with the request
-            interstitialAd.LoadAd(adRequest);
+            // Load the Interstitial with the request.
+            _interstitialAd.LoadAd(adRequest);
         }
 
         public void ShowInterstitialAd()
         {
-            if (interstitialAd.IsLoaded())
+            if (_interstitialAd.IsLoaded())
             {
-                interstitialAd.Show();
+                _interstitialAd.Show();
                 IsInterstitialAdReady = false;
-
-                RequestInterstititalAd();
             }
             else
-            {
-                Debug.LogError("Interstitial ad not loaded.");
-            }
+                Debug.LogError($"{TAG}: Interstitial ad not loaded.");
+            RequestInterstitialAd();
         }
 
         private void InterstitialAd_OnAdLoaded(object sender, System.EventArgs e)
         {
             IsInterstitialAdReady = true;
 
-            // Uncoment to debug
-            Debug.Log("Interstitial ad loaded.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Interstitial ad loaded.");
         }
+        
         #endregion
 
         #region Rewarded Ad Methods
-        // A rewarded ad ussually takes 60s to show
+        
+        /// <summary>
+        /// A rewarded ad usually takes 60s to show.
+        /// </summary>
         public void RequestRewardedAd()
         {
-            // Uncoment to debug
-            //Debug.Log("Requesting rewarded add.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Requesting rewarded add.");
 
             try
             {
-                // Initialize an interstitial add
-                rewardedAd = new RewardedAd(interstitialUnitId);
+                // Initialize an Interstitial add
+                _rewardedAd = new RewardedAd(interstitialUnitId);
 
                 // Create an empty ad request
                 AdRequest adRequest = new AdRequest.Builder().Build();
 
                 // Called when an ad request has successfully loaded
-                rewardedAd.OnAdLoaded += RewardedAd_OnAdLoaded;
+                _rewardedAd.OnAdLoaded += RewardedAd_OnAdLoaded;
 
                 // Called when an ad request has been closed
-                rewardedAd.OnAdClosed += RewardedAd_OnAdClosed;
+                _rewardedAd.OnAdClosed += RewardedAd_OnAdClosed;
 
                 // Called when the user should be rewarded for interacting with the ad
-                rewardedAd.OnAdDidRecordImpression += RewardedAd_OnAdDidRecordImpression;
-                rewardedAd.OnUserEarnedReward += RewardedAd_OnUserEarnedReward;
+                _rewardedAd.OnAdDidRecordImpression += RewardedAd_OnAdDidRecordImpression;
+                _rewardedAd.OnUserEarnedReward += RewardedAd_OnUserEarnedReward;
 
-                // Load the interstitial with the request
-                rewardedAd.LoadAd(adRequest);
+                // Load the Interstitial with the request
+                _rewardedAd.LoadAd(adRequest);
             }
             catch
             {
-                Debug.Log("Error loading rewarded.");
+                Debug.LogError($"{TAG}: Error loading rewarded ad.");
             }
         }
 
         private void RewardedAd_OnUserEarnedReward(object sender, Reward e)
         {
-            // Uncoment to debug
-            Debug.Log("Player rewarded.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Player rewarded.");
 
             // Trigger reward event
             IsShowingRewardedAd = false;
@@ -166,53 +215,56 @@ namespace DevPenguin.Ads
 
         private void RewardedAd_OnAdDidRecordImpression(object sender, System.EventArgs e)
         {
-            // Uncoment to debug
-            Debug.Log("Player rewarded after impression.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Player rewarded after impression.");
 
             // Trigger reward event
             IsShowingRewardedAd = false;
             if (OnReceiveReward != null)
                 OnReceiveReward();
             else
-                Debug.Log("Empty OnReceiveReward call");
+                Debug.LogError($"{TAG}: Empty OnReceiveReward call");
         }
 
         private void RewardedAd_OnAdClosed(object sender, System.EventArgs e)
         {
-            // Uncoment to debug
-            Debug.Log("Player closed rewarded.");
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Player closed rewarded.");
 
             // Trigger reward event
-            if (shouldRewardOnAdClosed)
+            if (shouldRewardOnAdClosedBeforeFinish)
                 OnReceiveReward();
-
             IsShowingRewardedAd = false;
             RequestRewardedAd();
         }
 
         public void ShowRewardedAd()
         {
-            if (rewardedAd.IsLoaded())
+            if (_rewardedAd.IsLoaded())
             {
-                rewardedAd.Show();
+                _rewardedAd.Show();
                 IsRewardedAdReady = false;
                 IsShowingRewardedAd = true;
+                ResetRewardStartTime();
             }
             else
-            {
-                Debug.LogError("Rewarded ad not loaded.");
-            }
+                Debug.LogError($"{TAG}: Rewarded ad not loaded.");
         }
 
         private void RewardedAd_OnAdLoaded(object sender, System.EventArgs e)
         {
-            // Uncoment to debug
-            Debug.Log("Rewarded ad loaded.");
-
             IsRewardedAdReady = true;
+            
+            // Debug.
+            if (areLogsEnabled)
+                Debug.Log($"{TAG}: Rewarded ad loaded.");
         }
+        
         #endregion
 
         #endregion
+        
     }
 }
